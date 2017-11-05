72f41ee3cd37b2acb1b99606012e548cf41a9f0e
CH <CH@10.68.101.212>
2017-11-05 15:30:41 +0100

    Tests hier hinzugefuegt (Test nachtraeglich in dieses package verschoben).

--- src/klosebrothers/JUnitTest.java
+++ src/klosebrothers/JUnitTest.java
@@ -1,36 +1,118 @@
 package klosebrothers;
 
 import static org.junit.Assert.*;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
+import static org.hamcrest.CoreMatchers.*;
 import org.junit.Test;
 
 public class JUnitTest {
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
+    @Test
+    public void threeThousandNineHundredAndNinetyNine() {
+        assertRomanNumeralEquals("MMMCMXCIX", 3999);
+    }
 
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
+    @Test
+    public void oneThousand() {
+        assertRomanNumeralEquals("M", 1000);
+    }
 
-	@Before
-	public void setUp() throws Exception {
-	}
+    @Test
+    public void nineHundred() {
+        assertRomanNumeralEquals("CM", 900);
+    }
 
-	@After
-	public void tearDown() throws Exception {
-	}
+    @Test
+    public void fiveHundred() {
+        assertRomanNumeralEquals("D", 500);
+    }
+
+    @Test
+    public void fourHundred() {
+        assertRomanNumeralEquals("CD", 400);
+    }
+
+    @Test
+    public void threeHundredAndNinetyNine() {
+        assertRomanNumeralEquals("CCCXCIX", 399);
+    }
+
+    @Test
+    public void ninety() {
+        assertRomanNumeralEquals("XC", 90);
+    }
+
+    @Test
+    public void eightyNine() {
+        assertRomanNumeralEquals("LXXXIX", 89);
+    }
+
+    @Test
+    public void sixty() {
+        assertRomanNumeralEquals("LX", 60);
+    }
 
 	@Test
-	public void test() {
-		
-		
-		fail("Not yet implemented");
+	public void fifty() {
+		assertRomanNumeralEquals("L", 50);
 	}
+	
+	@Test
+	public void forty() {
+		assertRomanNumeralEquals("XL", 40);
+	}
+	
+	@Test
+	public void fourtyOne() {
+		assertRomanNumeralEquals("XLI", 41);
+	}
+	
+	@Test
+	public void one() {
+		assertRomanNumeralEquals("I", 1);
+	}
+	
+	@Test
+	public void two() throws Exception {
+		assertRomanNumeralEquals("II", 2);
+	}
+	
+	@Test
+	public void five() {
+		assertRomanNumeralEquals("V", 5);
+	}
+	
+	@Test
+	public void four() {
+		assertRomanNumeralEquals("IV", 4);
+	}
+	
+	@Test
+	public void six() {
+		assertRomanNumeralEquals("VI", 6);
+	}
+	
+	@Test
+	public void nine() {
+		assertRomanNumeralEquals("IX", 9);
+	}
+	
+	@Test
+	public void ten() {
+		assertRomanNumeralEquals("X", 10);
+	}
+	
+	@Test
+	public void fourteen() {
+		assertRomanNumeralEquals("XIV", 14);
+	}
+	
+	@Test
+	public void thirtyNine() {
+		assertRomanNumeralEquals("XXXIX", 39);
+	}
+
+    private static void assertRomanNumeralEquals(String roman, int arab) {
+        assertThat(roman, is(FromArabicToRome.fromArabic(arab)));
+}
 
 }
